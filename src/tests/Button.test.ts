68a92756d09c27d4d0dc0179f371d847a88396b3
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-10 19:12:17 -0300

    feat: Button tests

---
+++ src/tests/Button.test.ts
@@ -0,0 +1,68 @@
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { Button } from '../components';
+
+describe('Components', () => {
+  let container: Element | null = null;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    if (container) {
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    }
+  });
+
+  describe('<Button />', () => {
+    let button: HTMLButtonElement | null | undefined;
+
+    const onClick = jest.fn();
+
+    beforeEach(async () => {
+      act(() => render(
+        Button({
+          name: 'test',
+          handleClick: onClick,
+          className: 'test',
+        }),
+        container
+      ));
+      button = container?.querySelector('button');
+    });
+  
+    afterEach(() => button = null);
+  
+    it('should render button\'s properties correctly', () => {
+      expect(button?.getAttribute('type')).toBe('button');
+      expect(button?.getAttribute('name')).toBe('test');
+      expect(button?.getAttribute('class')).toBe('test');
+    });
+
+    it('should render button\'s inner text correctly', () => {
+      expect(button?.textContent).toBe('test');
+    });
+
+    it('should not render a disabled and clickable button', () => {
+      expect(button).not.toBeDisabled();
+
+      act(() => button?.click());
+
+      expect(onClick).toHaveBeenCalled();
+    });
+
+    it('should render a disabled and unclickable button', () => {
+      expect(button).not.toBeDisabled();
+      button && (button.disabled = true);
+      expect(button).toBeDisabled();
+
+      act(() => button?.click());
+
+      expect(onClick).not.toHaveBeenCalled();
+    });
+  });
+});
