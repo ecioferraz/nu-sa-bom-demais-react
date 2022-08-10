71972b643184579c92ced6eda757fa54eddd6aa0
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-10 19:38:26 -0300

    feat: iTextCard

---
+++ src/interfaces/iTextCard.tsx
@@ -0,0 +1,5 @@
+interface iTextCard {
+  text: string,
+}
+
+export default iTextCard;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,9 @@
 import iButton from './iButton';
+import iTextCard from './iTextCard';
 import iTextInput from './iTextInput';
 
 export type {
   iButton,
+  iTextCard,
   iTextInput,
 };
