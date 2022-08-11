634486a05b62b81148f5422b8e73fa6fae8dfa1d
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-11 10:55:59 -0300

    refactor: ImageCard index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import Button from './Button';
+import ImageCard from './ImageCard';
 import RadioInput from './RadioInput';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
 export {
   Button,
+  ImageCard,
   RadioInput,
   TextCard,
   TextInput,
--- src/tests/components/ImageCard.test.ts
+++ src/tests/components/ImageCard.test.ts
@@ -1,6 +1,6 @@
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
-import ImageCard from '../../components/ImageCard';
+import { ImageCard } from '../../components';
 
 describe('Components', () => {
   let container: Element | null = null;
