c14d6db4b88dbac977e6e500993344f4e0eddf43
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-10 21:24:43 -0300

    feat: SelectInput

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Button from './Button';
+import SelectInput from './SelectInput';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
 export {
   Button,
+  SelectInput,
   TextCard,
   TextInput,
 };
