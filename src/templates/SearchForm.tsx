8af7b04d2602ee25178377cbc342d9ac289a300e
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-12 17:06:17 -0300

    style: SearchForm

--- src/templates/SearchForm.tsx
+++ src/templates/SearchForm.tsx
@@ -3,6 +3,8 @@ import { CheckboxInput, Loading, RangeInput, TextInput } from '../components';
 import { iSearchForm } from '../interfaces';
 import Categories from './Categories';
 
+import '../styles/SearchForm.css';
+
 const DISCOUNT_OPTION = ['On sale'];
 
 export default function SearchForm({
@@ -17,32 +19,38 @@ export default function SearchForm({
   setPriceRange,
 }: iSearchForm) {
   return (
-    <form>
-      <TextInput
-        name="search-input"
-        className="search-input"
-        type='search'
-        value={ searchInput }
-        handleChange={ setSearchInput }
-        placeholder="Search"
-      />
+    <>
+      <form className="search-form">
+        <fieldset>
+          <TextInput
+            name="search-input"
+            className="search-input"
+            type='search'
+            value={ searchInput }
+            handleChange={ setSearchInput }
+            placeholder="Search"
+          />
+          <section>
+            <CheckboxInput
+              className="discount-input"
+              options={ DISCOUNT_OPTION }
+              type="checkbox"
+              handleClick={ setWithDiscount }
+              value={ withDiscount }
+            />
+            <RangeInput
+              value={ priceRange }
+              handleChange={ setPriceRange }
+            />
+          </section>
+        </fieldset>
+      </form>
       {
         isLoading ? <Loading /> : <Categories
           adjectives={ categories }
           handleChange={ setSelectedCategory }
         />
       }
-      <CheckboxInput
-        className="discount-input"
-        options={ DISCOUNT_OPTION }
-        type="checkbox"
-        handleClick={ setWithDiscount }
-        value={ withDiscount }
-      />
-      <RangeInput
-        value={ priceRange }
-        handleChange={ setPriceRange }
-      />
-    </form>
+    </>
   );
 }
