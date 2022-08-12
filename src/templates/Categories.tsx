e904890209d1d831d0bc926ae89b0a0f81953509
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-12 17:06:05 -0300

    style: Categories

--- src/templates/Categories.tsx
+++ src/templates/Categories.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { RadioInput } from '../components';
 import { iCategories } from '../interfaces';
 
+import '../styles/Categories.css';
+
 export default function Categories({
   adjectives: categories,
   handleChange,
 }: iCategories) {
   return (
-    <aside>
+    <aside className="categories">
       <h3>Categories:</h3>
       <RadioInput
-        className="categories"
+        className="side-"
         name="categories"
         handleChange={ handleChange }
         type="radio"
