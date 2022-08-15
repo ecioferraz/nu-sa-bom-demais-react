200b5235414771ce05bca0ace853c4954a629051
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-15 11:36:23 -0300

    feat: Cart button

--- src/templates/SearchForm.tsx
+++ src/templates/SearchForm.tsx
@@ -4,6 +4,9 @@ import { iSearchForm } from '../interfaces';
 import Categories from './Categories';
 
 import '../styles/SearchForm.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const DISCOUNT_OPTION = ['On sale'];
 
@@ -19,36 +22,42 @@ export default function SearchForm({
   setPriceRange,
 }: iSearchForm) {
   return (
-    <>
-      <form className="search-form">
-        <TextInput
-          name="search-input"
-          className="search-input"
-          type='search'
-          value={ searchInput }
-          handleChange={ setSearchInput }
-          placeholder="Search"
-        />
-        <section className="search-options">
-          <CheckboxInput
-            className="discount-input"
-            options={ DISCOUNT_OPTION }
-            type="checkbox"
-            handleClick={ setWithDiscount }
-            value={ withDiscount }
-          />
-          <RangeInput
-            value={ priceRange }
-            handleChange={ setPriceRange }
-          />
-        </section>
-      </form>
+    <section>
       {
         isLoading ? <Loading /> : <Categories
           adjectives={ categories }
           handleChange={ setSelectedCategory }
         />
       }
-    </>
+      <form className="search-header">
+        <div className="search-form">
+          <TextInput
+            name="search-input"
+            className="search-input"
+            type='search'
+            value={ searchInput }
+            handleChange={ setSearchInput }
+            placeholder="Search"
+          />
+          <section className="search-options">
+            <CheckboxInput
+              className="discount-input"
+              options={ DISCOUNT_OPTION }
+              type="checkbox"
+              handleClick={ setWithDiscount }
+              value={ withDiscount }
+            />
+            <RangeInput
+              value={ priceRange }
+              handleChange={ setPriceRange }
+            />
+          </section>
+        </div>
+        <Link to="/cart" className="cart-btn">
+          <span>Ver carrinho </span>
+          <FontAwesomeIcon icon={ faCartShopping } size={ '2x' } />
+        </Link>
+      </form>
+    </section>
   );
 }
